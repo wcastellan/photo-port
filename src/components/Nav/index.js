b85fa3698763f91592277e6cf3dea49b12683c74
Wyatt C <wcastellan@yahoo.com>
2022-02-21 17:41:44 -0500

    make slight changes to the index.js in the Nav folder

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -21,36 +21,34 @@ function categorySelected(name) {
 function Nav() {
 
     return (
-        <header>
+        <header className="flex-row px-1">
             <h2>
-                <a href='/'>
-                    <span role="img" aria-label='camera'> 📸</span> Oh Snap!
+                <a href="/">
+                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
                 </a>
             </h2>
             <nav>
-                <ul className='flex-row'>
-                    <li className='mx-2'>
-                        <a href='#about'>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        <a href="#about">
                             About me
                         </a>
                     </li>
-                    <li>
+                    <li className={"mx-2"}>
                         <span>Contact</span>
                     </li>
                     {categories.map((category) => (
-                        <li 
-                            className="mx-1"
-                            key={category.name}
-                            >
-                                <span onClick={() => categorySelected(category.name)} >
-                                    {category.name}
-                                </span>
-                            </li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
-    );
+                        <li className="mx-1" key={category.name} >
+                            <span onClick={() => categorySelected(category.name)} >
+                                {category.name}
+                            </span>
+                        </li>
+                    ))
+                }
+            </ul>
+        </nav>
+    </header>
+);
 }
 
 export default Nav;
